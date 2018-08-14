7f58d7bb2938eb57b610a7fae8417e4102a56f2d
Andylytical <aloftus@gmail.com>
2018-08-13 23:42:00 -0500

    generic dkbuild

--- dkbuild.sh
+++ dkbuild.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-set -x
-
-
-NAME=brewpi-backup
 USER=andylytical
 TS=$( date +%s )
+
+fatal() {
+    echo "Error: $*"
+    exit 1
+}
+
+[[ $# -eq 1 ]] || fatal "expected 1 argument, got '$#'"
+[[ -d "$1" ]] || fatal "not a directory: '$1'"
+NAME="$1"
+
+set -x
 
 docker build \
     -f $NAME/Dockerfile \
